b93cc174bfc5b57482abd33ba9d49c06bb487dea
Shane <zhenren1098@gmail.com>
2026-01-07 16:01:47 +0800

    Initial commit

---
+++ toDo-list.js
@@ -0,0 +1,8 @@
+const toDoList = [];
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+}
+document.querySele
+// 1. Create Array to store To Dos
+//2. When we click add, get text from text box
