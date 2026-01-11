9646a9918d02011a78b7a4065e2dc62e9aba7392
Shane <zhenren1098@gmail.com>
2026-01-11 21:59:15 +0800

    Add dynamic rendering for to-do list items
    Introduced a renderTodo function to display to-do items dynamically in the HTML. Updated addTodo to re-render the list after adding a new item, and added a container in the HTML for the to-do list.

--- toDo-list.js
+++ toDo-list.js
@@ -1,6 +1,31 @@
 const toDoList = [];
 
+renderTodo();
+
+function renderTodo() {
+
+  let todoListHTML = '';
+
+  for (i = 0; i < toDoList.length; i++) {
+    const todo = toDoList[i];
+    //access task from array and stoe in todo variable
+    const html = `<p> ${todo}</p>`;
+    // and put it in html code then store it in html varaiable.
+    todoListHTML += html;
+    //accumulate the tasks
+  }
+
+  console.log(todoListHTML);
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+  //gets the .js-todo-list from html file and change the content into todoListHTML.
+
+}
+
+
 function addTodo() {
+
   const inputElement = document.querySelector('.js-name-input');
   //Get the whole input
 
@@ -13,6 +38,8 @@ function addTodo() {
 
   inputElement.value = '';
   //every time we click add, the text box resets.
+
+  renderTodo();
 
 }
 
