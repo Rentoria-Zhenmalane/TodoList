957448214017b72d3655f93cdcc40c1f43b2e2ee
Shane <zhenren1098@gmail.com>
2026-01-28 23:57:23 +0800

    Refactor event handling and update UI selectors
    Replaced inline onclick handlers with event listeners for add and delete buttons. Updated button classes in HTML and JS for better query selection. Improved code readability by switching from for loop to forEach in renderTodo().

--- toDo-list.js
+++ toDo-list.js
@@ -12,27 +12,29 @@ renderTodo();
 function renderTodo() {
 
   let todoListHTML = '';
+  toDoList
 
-  for (let i = 0; i < toDoList.length; i++) {
-    const todoObject = toDoList[i];
+  toDoList.forEach(todoObject => {
+    //const todoObject = toDoList[i];
     //access task from array and store in todo variable
+    //But since we use forEach loop, and it already have the variable toDoObject then this is unnecessary.
 
     //const dueDate = todoObject.dueDate;
     //const name = todoObject.name; 
     const { name, dueDate } = todoObject; //called as Destructuring, a shortcut from above.
 
-
     //need to separate the three elements using div so the CSS grid could work.
     //Why div? because div doesn't have a default style, so it's easier to style.
     const html = `
-      <div> ${name} </div>
-      <div>${dueDate} </div> 
-      <button class="delete-button" onclick=
-        "toDoList.splice(${i}, 1);
-        renderTodo();
-      ">Delete</button>
+        <div> ${name} </div>
+        <div>${dueDate} </div> 
+        <button class="delete-button js-delete-button">Delete</button>
 
-    `;
+      `;
+
+    //${index} → the position of the todo item in the array (provided by forEach).1 → tells JavaScript to remove exactly one element at that position.
+    //So if you click the second delete button then index will become 1 --> hence, toDoList.splice(${1}, 1);
+
     //Template literals (backticks) allow embedding variables in strings.
     //Generating HTML using backticks is a technique that generates html inside Javascript,
     //Why call renderToDo() inside html? To update the UI everytime you hit delete button. This is called manual rendering.
@@ -40,14 +42,26 @@ function renderTodo() {
 
     todoListHTML += html;
     //accumulate the tasks
-  }
-
+  })
 
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
   //gets the .js-todo-list from html file and change the content into todoListHTML.
 
+  document.querySelectorAll('.js-delete-button')
+    .forEach((index) => {
+      addEventListener('click', () => {
+        toDoList.splice(index, 1);
+        renderTodo();
+      })
+    })
+
 }
+
+document.querySelector('.js-add-button')
+  .addEventListener('click', () => {
+    addTodo();
+  });
 
 
 function addTodo() {
